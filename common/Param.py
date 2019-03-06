183a0fd554be528acc018b15ef64df41656619e8
xiongyi <xiongyi@lenovocloud.com>
2019-03-06 10:50:13 +0800

    增加注释

--- common/Param.py
+++ common/Param.py
@@ -15,7 +15,7 @@ class Param:
 		if not path is None and not sheet is None:
 			self.sheet = self.excel.sheet_by_name(sheet)
 
-
+#打开excel文件
 	def reopen(self, path, sheet = None):
 		try:
 			self.excel = xlrd.open_workbook(path)
@@ -28,25 +28,26 @@ class Param:
 
 		return True
 
-
+#切换sheet
 	def turn(self, sheet):
 		self.sheet = self.excel.sheet_by_name(sheet)
 
-
+#读sheet中单元格的数据
 	def read(self, row, col):
 		try:
-			ctype = self.sheet.cell(row, col).ctype
-			if ctype == 0 or ctype == 5:
+			ctype = self.sheet.cell(row, col).ctype  #单元格数据类型
+			if ctype == 0 or ctype == 5:  # 0或5 是空
 				return None
-			if ctype == 2:
+			if ctype == 2: #整形
 				return int(self.sheet.cell(row, col).value)
 			return self.sheet.cell(row, col).value
 		except BaseException:
 			return None
 
 
+#转换得到的值，如果是数组就转换成数组，否则直接返回
 	def transValue(self, value):
-		if (isinstance(value, str) or isinstance(value, unicode)) and len(value) > 2 and value[0] == '[' and value[len(value) -1] == ']':
+		if isinstance(value, str)  and len(value) > 2 and value[0] == '[' and value[len(value) -1] == ']':
 			value = value.lstrip('[')
 			value = value.rstrip(']')
 			value = value.split(',')
@@ -56,16 +57,18 @@ class Param:
 		return value
 
 
+#获得用例数量
 	def caseCount(self):
 		return len(self.excel.sheet_names())
 
 
+#切换用例
 	def switchCase(self, caseIndex):
 		self.sheet = self.excel.sheet_by_index(caseIndex)
 
 		return self.sheet.name;
 
-
+#读取调用方法、地址、执行次数
 	def readStep(self, startFrom = 1):
 		method = self.read(startFrom, 0)
 		url = self.read(startFrom, 1)
@@ -81,12 +84,13 @@ class Param:
 		return address
 
 
+#读取参数
 	def readData(self, startFrom = 1):
-		row = startFrom
+		row = startFrom #开始读的行
 		data = {}
 		while True:
 			name = self.read(row, 2)
-			if name is None or name == "":
+			if name is None or name == "": #没有参数，表示读取结束
 				break
 			value = self.read(row, 3)
 			data[name] = self.transValue(value)
@@ -94,7 +98,7 @@ class Param:
 
 		return data;
 
-
+#读期望值
 	def readCheck(self, startFrom = 1):
 		row = startFrom
 		check = {}
