602ae621b6ce405047739b5eaf894743281d44ea
xiongyi <xiongyi@lenovocloud.com>
2019-11-03 23:24:30 +0800

    添加解析类

---
+++ common/Parser.py
@@ -0,0 +1,16 @@
+# encoding=utf-8
+import json
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def index(self):
+        pass
+
+    def menus(self):
+        pass
+
+    def content(self):
+        pass
+
